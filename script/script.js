4936d1b1ab9eb6c912369417a2ab105fa5862adb
HusakOleh <husakoleh91@gmail.com>
2022-05-17 12:13:27 +0300

    create git

---
+++ script/script.js
@@ -0,0 +1,4 @@
+document.querySelector('#drop').onclick = function click() {
+    var nav = document.querySelector('.nav');
+    nav.style.display == 'block' ? nav.style.display = 'none' : nav.style.display = 'block';
+}
